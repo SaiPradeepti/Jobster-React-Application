99a1a68744f90b5162b6e33c8accf874e147fe61
Sai Pradeepti <saipradeepti14@gmail.com>
2022-11-15 09:21:07 +0530

    Adding pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react'
-import Landing from './pages/Landing'
-import styled from 'styled-components'
-
-const Button = styled.button`
-background: red;
-color:white;
-font-size: 2rem;
-`;
-
-const SecondButton = styled.button`
-background: pink;
-`
+import {Landing, Error, Dashboard, Register} from './pages'
+// import styled from 'styled-components'
 
 const App = () => {
   return (
